49e145c617471d44abf7a4bb89b736a7bd404b18
Gerson Aranibar <aranibargerson28@gmail.com>
2022-05-13 21:16:20 -0500

    fix main page

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicService } from 'src/app/services/public.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
 })
 export class IndexComponent implements OnInit {
-  constructor() {}
+  public discounts: any = undefined;
 
-  ngOnInit(): void {}
+  constructor(private publicService: PublicService) {}
+
+  ngOnInit(): void {
+    this.publicService.init_menu();
+    this.get_discount();
+  }
+
+  get_discount() {
+    this.publicService.get_discount_active().subscribe({
+      next: (res) => {
+        this.discounts = res ? res.data[0] : undefined;
+      },
+    });
+  }
 }
